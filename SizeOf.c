0a510fc6966ed9057ed32cf0eb41d0142a5d37fe
Prajjwal Pathak <prajjwalpathak.asm@gmail.com>
2020-09-11 10:10:39 +0530

    Added SizeOf.c program

---
+++ SizeOf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+    int a;
+    float b;
+    double c;
+    char d;
+    printf("\nSize of int: %d",sizeof(a));
+    printf("\nSize of float: %d",sizeof(b));
+    printf("\nSize of double: %d",sizeof(c));
+    printf("\nSize of char: %d",sizeof(d));
+
+    return 0;
+}
