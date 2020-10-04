194a40104ecc07ab897e7ee628ffa39ca22edd9c
hsteg420 <72036132+hsteg420@users.noreply.github.com>
2020-10-04 09:46:01 +0530

    Updated the code for better result
    a more user friendly and easy program

--- BubbleSort.c
+++ BubbleSort.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
-
 int main()
 {
   int array[100], num, i, j, swap;
-
   printf("Enter number of elements\n");
   scanf("%d", &num);
-
   printf("Enter %d integers:\n", num);
-
   for (i = 0; i < num; i++)
     scanf("%d", &array[i]);
-
   for (i = 0 ; i < num - 1; i++)
   {
     for (j = 0 ; j < num - i - 1; j++)
@@ -21,14 +16,9 @@ int main()
         swap       = array[j];
         array[j]   = array[j+1];
         array[j+1] = swap;
-      }
-    }
-  }
-
+      }}}
   printf("Sorted list:\n");
-
   for (i = 0; i < num; i++)
      printf("%d\n", array[i]);
-
   return 0;
 }
