b1c560339b7b1922f3f0f2387a91660fcd3c3028
Prajjwal Pathak <prajjwalpathak.asm@gmail.com>
2020-10-06 10:25:54 +0530

    Updated CharToASCII.c program

---
+++ CharToASCII.c
@@ -0,0 +1,13 @@
+//Program to convert character to ASCII value
+
+#include<stdio.h>
+
+int main()
+{
+    char c;
+    printf("Enter char: ");
+    scanf("%c", &c);
+    printf("ASCII value is: %d", c);
+
+    return 0;
+}
--- NumTooAscii.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-void main()
-{
-    char inp;
-    printf("Enter char: ");
-    scanf("%c", &inp);
-    printf("ASCII value is: %d", inp);
-}
