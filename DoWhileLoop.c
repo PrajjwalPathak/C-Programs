8d3c88b6e42e46033ce9b736e6ea4f491db93004
Prajjwal Pathak <prajjwalpathak.asm@gmail.com>
2020-10-08 10:15:03 +0530

    Updated few program

--- DoWhileLoop.c
+++ DoWhileLoop.c
@@ -1,8 +1,13 @@
+//Program to demostrate the use of Do While loop
+
 #include<stdio.h>
+
 int main() {
     int i=0;
     do {
         printf("Spider-Man\n");
         i++;
     } while(i<10);
+
+    return 0;
 }
