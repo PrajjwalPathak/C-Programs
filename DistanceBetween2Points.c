65a8feb0dd8d35abeac957b1ca3df45a30660df7
Prajjwal Pathak <prajjwalpathak.asm@gmail.com>
2020-10-14 10:16:28 +0530

    Updated few programs

--- DistanceBetween2Points.c
+++ DistanceBetween2Points.c
@@ -1,14 +1,16 @@
+//Program to find the distance between two points
+
 #include <stdio.h>
 #include <math.h>
 
-int main()
-{
+int main() {
+
     float x1,y1;
     float x2,y2;
     float sub1, sub2, sumAll = 0;
-    printf("Input the first point | X and Y \n");
+    printf("Input the first point | X1 and Y1 : ");
     scanf("%f %f",&x1,&y1);
-    printf("Input the second point | X2 and Y2 \n");
+    printf("Input the second point | X2 and Y2 : ");
     scanf("%f %f",&x2,&y2);
     
     
@@ -18,8 +20,7 @@ int main()
     
     float rz = sqrt(sumAll);
     
-    printf("%0.4f",rz);
+    printf("%0.2f",rz);
     
-
     return 0;
 }
