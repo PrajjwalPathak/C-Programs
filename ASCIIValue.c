87bfa776e1e9e989f7457ad44218ff88b9c318e3
prajjwalpathak <prajjwalpathak.asm@gmail.com>
2021-01-04 20:29:10 +0530

    Add ASCIIValue.c

---
+++ ASCIIValue.c
@@ -0,0 +1,12 @@
+//Program to find ASCII value of character
+
+#include<stdio.h>
+
+int main() {
+    char i;
+    printf("Enter a character: ");
+    scanf("%c",&i);
+    printf("The ASCII value is: %d",(int)i);
+    
+    return 0;
+}
